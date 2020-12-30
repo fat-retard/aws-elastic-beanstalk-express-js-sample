0b68141503c1afdb382c8a3b524c9ae99b5579ad
Sneed <spz@firemail.cc>
2020-12-30 14:48:22 -0500

    changed line 5

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('something other than hello world!'));
+app.get('/', (req, res) => res.send('poop'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
